10d591776c549e12a5dbe7e1375de75daa714548
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2021-09-09 00:41:07 -0500

    BFS

---
+++ queue.h
@@ -0,0 +1,25 @@
+#ifndef QUEUE_H_ /* Include guard */
+#define QUEUE_H_
+
+// A structure to represent a queue
+struct Queue {
+  int front, rear, size;
+  unsigned capacity;
+  int *array;
+};
+
+struct Queue *createQueue(unsigned capacity);
+
+int isFull(struct Queue *queue);
+
+int isEmpty(struct Queue *queue);
+
+void enqueue(struct Queue *queue, int item);
+
+int dequeue(struct Queue *queue);
+
+int front(struct Queue *queue);
+
+int rear(struct Queue *queue);
+
+#endif
